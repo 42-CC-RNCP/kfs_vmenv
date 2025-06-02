e050a1876324cb11c618eb13d97c352ccdbeb20e
Lea Yeh <lea.yeh.ml@gmail.com>
2025-06-02 02:57:10 +0200

    feat: Add scripts for host pc

---
+++ scripts/setup_host_env.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+# install build tools and dependencies for building the kernel
+
+set -e
+
+echo "🔍 Updating package list..."
+sudo apt update
+
+echo "📦 Installing build tools and dependencies..."
+sudo apt install -y \
+  build-essential \
+  libncurses-dev \
+  grub-pc-bin grub-common \
+  parted \
+  xz-utils zlib1g-dev \
+  wget curl m4 git
+
+echo "🔧 Setting up the Unitial environment..."
+wget  -O- https://github.com/PeterDaveHello/Unitial/raw/master/setup.sh | bash
+
+echo "✅ Host environment setup completed!"

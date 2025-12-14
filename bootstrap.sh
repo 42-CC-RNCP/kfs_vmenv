85ececb41e772ca729ec147fa45cb39a4de1f588
Lea Yeh <lea.yeh.ml@gmail.com>
2025-12-14 15:42:38 +0100

    fix: Avoid to use host tool:glibc

--- bootstrap.sh
+++ bootstrap.sh
@@ -44,7 +44,10 @@ declare -A STEPS=(
   [build_kernel]="./scripts/build_kernel.sh"
   [setup_bootloader]="./scripts/setup_bootloader.sh"
   [init_lfs]="./scripts/init_lfs.sh"
-  [build_toolchain]="sudo -u lfs env PATH=/tools/bin:$PATH BASEDIR=$BASEDIR LFS=$LFS LFS_TGT=$LFS_TGT BUILD_DIR=$BUILD_DIR bash ./scripts/build_lfs_core.sh"
+  [build_toolchain]="sudo -u lfs env -i HOME=/home/lfs TERM=\"$TERM\" \
+    LFS=\"$LFS\" LFS_TGT=\"$LFS_TGT\" BASEDIR=\"$BASEDIR\" BUILD_DIR=\"$BUILD_DIR\" \
+    PATH=\"/usr/bin:/bin:$LFS/tools/bin\" \
+    /bin/bash ./scripts/build_lfs_core.sh"
   [mount_lfs]="./scripts/mount_lfs.sh"
   [fix_toolchain]="chroot_exec fix_toolchain.sh"
   [build_pass2]="chroot_exec build_pass2.sh"

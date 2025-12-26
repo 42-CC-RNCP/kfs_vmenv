bade45deb1352a549256bd1164cd44f0f7a52bcb
Lea Yeh <lea.yeh.ml@gmail.com>
2025-12-26 15:53:48 +0100

    chore: Skip if the node existed

--- scripts/mount_lfs.sh
+++ scripts/mount_lfs.sh
@@ -6,8 +6,8 @@ mkdir -pv "$LFS"/{dev,proc,sys,run}
 mkdir -pv "$LFS/scripts"
 
 # 6.2.1: initial device nodes (on disk)
-mknod -m 600 "$LFS/dev/console" c 5 1
-mknod -m 666 "$LFS/dev/null"    c 1 3
+mknod -m 600 "$LFS/dev/console" c 5 1 || true
+mknod -m 666 "$LFS/dev/null"    c 1 3 || true
 
 # 6.2.2: bind-mount /dev
 mountpoint -q "$LFS/dev" || mount -v --bind /dev "$LFS/dev"

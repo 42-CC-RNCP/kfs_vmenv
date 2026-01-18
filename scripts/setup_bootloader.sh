6d019d22bd55f6281e4013b96383a16e793a12f5
Lea Yeh <lea.yeh.ml@gmail.com>
2026-01-18 17:54:37 +0100

    chore: Add debug message in grub

--- scripts/setup_bootloader.sh
+++ scripts/setup_bootloader.sh
@@ -17,7 +17,7 @@ insmod ext2
 set root=(hd0,1)
 
 menuentry "GNU/Linux, Linux 4.20.12-lyeh" {
-        linux   /vmlinuz-4.20.12-lyeh root=/dev/sda2 ro
+        linux   /vmlinuz-4.20.12-lyeh root=/dev/sda2 rw console=ttyS0,115200 debug loglevel=7
 }
 EOF
 

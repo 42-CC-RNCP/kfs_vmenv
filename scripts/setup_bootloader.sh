3c091c1422168176f6f8fa953ac9472c84e93d9b
Lea Yeh <lea.yeh.ml@gmail.com>
2026-01-18 17:27:03 +0100

    chore: Fix the typo of kernel name

--- scripts/setup_bootloader.sh
+++ scripts/setup_bootloader.sh
@@ -16,8 +16,8 @@ set timeout=5
 insmod ext2
 set root=(hd0,2)
 
-menuentry "GNU/Linux, Linux 4.20.12-lfs-8.4" {
-        linux   /boot/vmlinuz-4.20.12-lfs-8.4 root=/dev/sda2 ro
+menuentry "GNU/Linux, Linux 4.20.12-lyeh" {
+        linux   /boot/vmlinuz-4.20.12-lyeh root=/dev/sda2 ro
 }
 EOF
 
